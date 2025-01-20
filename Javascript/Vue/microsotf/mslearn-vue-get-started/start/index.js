8d0035280afe5ce60de411bf815e5519282fa807
iDankest <DankestMirror@hotmail.es>
2025-01-20 16:12:43 +0000

    Segunda leccion vue

---
+++ Javascript/Vue/microsotf/mslearn-vue-get-started/start/index.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      productName: 'Book a cruise to the moon',
+      productDescription:
+        'cruse to the moon in our luxurius. watch the astronauts at work',
+    };
+  },
+});
+app.mount('#app');
