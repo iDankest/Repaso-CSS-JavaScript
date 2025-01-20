20292fed8ab4574cf3ae52c2d6aba77c48942049
iDankest <DankestMirror@hotmail.es>
2025-01-20 11:45:10 +0000

    done

--- Javascript/SC6.js
+++ Javascript/SC6.js
@@ -8,3 +8,24 @@ const copy = [...fruts];
 copy[0] = '🍉';
 //Nueva manera de hacer una copia de un array sin modificar la original
 const newcopy = fruts.with(1, '🥑');
+
+// Nuevo metodo groupBy
+// ejemplo antiguo
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let grouped = {
+//   par: [],
+//   impar: [],
+// };
+// numbers.forEach((number) => {
+//   if (number % 2 === 0) {
+//     grouped.par.push(number);
+//   } else {
+//     grouped.impar.push(number);
+//   }
+// });
+
+//nuevo metodo
+const grouped = Object.groupBy(numbers, (number) => {
+  if (number % 2 === 0) return 'par';
+  return 'impar';
+});
